107372f4cbca88a97edc77f78237413adec149c5
Hongcuc-nguyen <cucb2306610@student.ctu.edu.vn>
2026-06-30 17:06:47 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app/controllers/contact.controller.js
+++ app/controllers/contact.controller.js
@@ -2,9 +2,12 @@ exports.create = (req, res) => {
     res.send({ message: "create handler" });
 };
 
+exports.findAll = (req, res) => {
+    res.send({ message: "findAll handler" });
 };
 
 exports.findOne = (req, res) => {
+    res.send({ message: "findOne handler" });
 };
 
 exports.update = (req, res) => {
